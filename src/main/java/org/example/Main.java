f91a64e68c40e2c380c96b725cf5257287079a3c
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-11-08 19:59:20 -0300

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+
+        //config de conexão
+
+        String url = "jdbc:mysql://localhost:3306/poo";
     }
 }
