1ce8d98047ca49f84a491f141f9e523cc4e9a5f0
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-11-08 20:09:02 -0300

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,30 @@
 package org.example;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
 
         //config de conexão
-
         String url = "jdbc:mysql://localhost:3306/poo";
+        String usuario = "root";
+        String senha = "";
+
+        //comando SQL
+        String sql = "INSERT INTO aluno (nome, endereco) VALUES (?, ?)";
+
+        try {
+            Connection conexao = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexão estabelecida com sucesso!");
+
+            //prepara uma instrução SQL
+            PreparedStatement statement = conexao.prepareStatement(sql);
+
+            statement.setString(1, "Leandro");
+            statement.setString(2, "Rua Aleatória, número 1");
+
+        }catch (SQLException erro) {
+            System.out.println();
+        }
     }
 }
